8e63484dd5bdd93944958d27b67414ed6b6c819b
User <user@its.me>
2023-02-23 14:03:19 +0100

    Initial commit

---
+++ src/test/java/online/javafun/dynamicstyle/DynamicStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.dynamicstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicStyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
